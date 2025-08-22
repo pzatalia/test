b9a86ff4b81a141bbf58942628588a845b4b89d1
natalia <napo004@student.kristiania.no>
2025-08-22 13:17:50 +0200

    skrevet kode

---
+++ romsnNumerals.text.js
@@ -0,0 +1,9 @@
+import { test, expect } from "vitest";
+
+test("1 in roman numerals is I", () => {
+  function romanNumerals(number) {
+    return undefined;
+  }
+
+  expect(romanNumerals(1)).toBe("I");
+});
